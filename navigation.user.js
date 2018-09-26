5b8211eb56993a6761625363cf2d4d187d3c1bb2
firefu <firefu@tauhead.com>
2018-09-26 08:12:00 +0100

    refactor so all functionality is easily disabled, and configuration is above all functions in source

--- navigation.user.js
+++ navigation.user.js
@@ -8,15 +8,6 @@
 // @require http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
-function show_hotel_room() {
-    // contributed by Perleone
-    if ( !window.location.pathname.match('^/area/hotel-rooms/enter-room') ) {
-        return;
-    }
-    $('.area-hotel-rooms h1').append( ' ⇒ ' + $('.zone-notice').text() ).css('background-color', 'blue');
-}
-
-
 function gs_taustation_enhance() {
     // show area links for the most common sub-areas
     $('#game_navigation_areas a[href="/travel/area/inn"]').parent('li').after('<li class="area "> <a style="padding-left: 2em" href="/travel/area/bar">Bar</a> / <a href="/area/hotel-rooms/enter-room">Room</a> / <a href="/travel/area/lounge">Lounge</a> </li>');
@@ -28,10 +19,10 @@ function gs_taustation_enhance() {
 
     // bond to credits conversion in the bank
     // $('.bond-to-credits').hide();
-    
+
     // bribe for an extra ration
     // $('a.btn-buy-rations').hide();
-    
+
     // Intelligence training
     // $('.btn-train-intelligence').hide();
     // $('.header-train-intelligence').hide();
@@ -46,15 +37,41 @@ function gs_taustation_enhance() {
     // hide area tutorial and area image to save space
     // $('.tutorial').hide(); // area tutorial
     // $('.area-hero').hide(); // area image
-  
+
     // make the Shop button less annoying
     $('.shop').removeClass('shop').addClass('bug-report')
-  
+
+    // show the number of the discreet work (counter per station)
+    show_discreet_counter();
+
+    // Highlight when safe in hotel-room
+    show_hotel_room();
 
     // new feature: Once the chat is shown,
     // clicking on "CHAT" will increase the size of the chat window
+    modify_chat();
+
+    //
+    // END OF USER-CONFIGURATION
+    //
+
     var is_fullpage_chat = false;
     var old_style;
+    function modify_chat() {
+        $('.chat-heading').bind('click', function () {
+            if (is_fullpage_chat) {
+                is_fullpage_chat = false;
+                normal_chat();
+            }
+            else {
+                is_fullpage_chat = true;
+                if (! old_style) {
+                    old_style = $('#chat').attr('style');
+                }
+                fullpage_chat();
+            }
+        });
+    }
 
     function fullpage_chat() {
         $('#chat').width($(document).width() - 40);
@@ -72,21 +89,6 @@ function gs_taustation_enhance() {
         $('.page-container').show();
     }
 
-    $('.chat-heading').bind('click', function () {
-        if (is_fullpage_chat) {
-            is_fullpage_chat = false;
-            normal_chat();
-        }
-        else {
-            is_fullpage_chat = true;
-            if (! old_style) {
-                old_style = $('#chat').attr('style');
-            }
-            fullpage_chat();
-        }
-    });
-
-    // show the number of the discreet work (counter per station)
     function show_discreet_counter() {
         // contributed by Perleone
         if ( !window.location.pathname.match('^(/area/discreet-work|/character/details/)') ) {
@@ -99,9 +101,15 @@ function gs_taustation_enhance() {
         }
     }
 
-    show_discreet_counter();
-    show_hotel_room();
+    function show_hotel_room() {
+        // contributed by Perleone
+        if ( !window.location.pathname.match('^/area/hotel-rooms/enter-room') ) {
+            return;
+        }
+        $('.area-hotel-rooms h1').append( ' ⇒ ' + $('.zone-notice').text() ).css('background-color', 'blue');
+    }
+
+
 
 }
 $(document).ready(gs_taustation_enhance);
-
