05c0119bd0c4dcd78ae23a77fbd0334fbed2c67b
Mark Schurman <m.schurman@f5.com>
2018-12-11 12:05:52 -0800

    Updated to support Firefox.
    Changes:
    1. Replaced GM_* methods with localStorage & local "add_css()" method.
         - If prior version of script stored data using legacy APIs, this copies that data into localStorage for reuse.
    
    2. Click handlers: In Firefox, jQuery didn't recognize "$(...).on(...)"; replaced them with "$(...).click(...)".
    
    3. Extra: The entire "Edit button" is now clickable, not just the text inside it.

--- hide_tasks.user.js
+++ hide_tasks.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         taustation_hide_tasks
 // @namespace    https://github.com/dot-sent
-// @version      0.2
+// @version      0.3
 // @description  Hide irrelevant career tasks
 // @author       Dot_sent
 // @match        https://alpha.taustation.space/area/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
-// @require http://code.jquery.com/jquery-3.3.1.min.js
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
+//
+// Note: The @grant lines above were added mainly to indicate that this script
+//       uses key/value storage & adds CSS styles. The actual GM_* methods
+//       themselves are deprecated, and no longer supported by GreaseMonkey.
+//
 
-GM_addStyle(`
+add_css(`
   .tau_station_hidden_tasks_btn_edit{
     border: 1px solid #13628b;
     background-color: #14374a;
@@ -24,15 +29,31 @@ GM_addStyle(`
     font-weight: 300;
     font-family: Electrolize,sans-serif;
     text-align: center;
+    cursor: pointer;
   }
   .tau_station_hidden_tasks_btn_edit span{
     margin: 3% 3% 3% 3%;
   }
 `);
 
+// For Chrome: If not set, check if we can load older state from GM_getValue().
+// (TamperMonkey (Chrome) still has legacy support for the GM_* methods; GreaseMonkey (Firefox) does not.)
+var legacy_hiddenTasksString = undefined;
+if (! localStorage.hasOwnProperty("tau_station_hidden_task_array")) {
+    try {
+        legacy_hiddenTasksString = GM_getValue("tau_station_hidden_task_array", "{}");
+        if (legacy_hiddenTasksString) {
+            console.log("hidden_tasks: Loaded prior data from GM_getValue(); using localStorage henceforth.");
+        }
+    }
+    catch (err) {
+        // Legacy API not supported in this browser; just use localStorage below.
+    }
+}
+
 var is_editing_visibility = 0;
-var hiddenTasksString = GM_getValue("tau_station_hidden_task_array", "{}");
-var hiddenTasks = hiddenTasksString == undefined || hiddenTasksString == "" || hiddenTasksString == "[]" ? {} : JSON.parse(hiddenTasksString);
+var hiddenTasksString = localStorage["tau_station_hidden_task_array"] || legacy_hiddenTasksString || "{}";
+var hiddenTasks = (hiddenTasksString == undefined || hiddenTasksString == "" || hiddenTasksString == "[]" ? {} : JSON.parse(hiddenTasksString));
 
 function taustation_hide_tasks() {
     $('div.tab-content-career').find("td[data-label='Task']").each(function(i,el){
@@ -42,36 +63,49 @@ function taustation_hide_tasks() {
     });
 }
 
-function toggle_task_visibility(evt) {
-    hiddenTasks[evt.data.task_title] = 1 - evt.data.state;
-    GM_setValue("tau_station_hidden_task_array", JSON.stringify(hiddenTasks));
+function toggle_task_visibility(evt, data) {
+    hiddenTasks[data.task_title] = 1 - data.state;
+    localStorage.setItem("tau_station_hidden_task_array", JSON.stringify(hiddenTasks));
     var elem = evt.target;
+    $(elem).html(data.state == 1 ? "Hide" : "Show");
     $(elem).off();
-    $(elem).on('click', { task_title: evt.data.task_title, state: 1 - evt.data.state}, toggle_task_visibility);
-    $(elem).html(evt.data.state == 1 ? "Hide" : "Show");
+    $(elem).click(function (evt) { toggle_task_visibility(evt, { task_title: data.task_title, state: 1 - data.state }); });
 }
 
 function toggle_edit_interface() {
     if (is_editing_visibility){
         is_editing_visibility = 0;
         $('.tau_station_show_task_container').remove();
-        $('#tau_station_hidden_task_btn_edit').html('Edit task visibility');
+        $('#tau_station_hidden_tasks_btn_edit_label').html('Edit task visibility');
         taustation_hide_tasks();
     } else {
         is_editing_visibility = 1;
-        $('#tau_station_hidden_task_btn_edit').html('Apply visibility settings');
+        $('#tau_station_hidden_tasks_btn_edit_label').html('Apply visibility settings');
         $('.tab-content-career table thead tr').append('<th class="tau_station_show_task_container">Hide/Show</th>');
         $('div.tab-content-career').find("td[data-label='Task']").each(function(i,el){
             var button_code = '<td class="tau_station_show_task_container"><a class="btn-control normal tau_station_show_hide_task">' + (hiddenTasks[el.innerHTML] == 1 ? 'Show' : 'Hide') + '</a></td>';
             $(el).parent().show().append(button_code);
-            $(el).parent().find('.tau_station_show_hide_task').on('click', { task_title: el.innerHTML, state: hiddenTasks[el.innerHTML] == 1 ? 1 : 0 }, toggle_task_visibility);
+            $(el).parent().find('.tau_station_show_hide_task').click(function (evt) { toggle_task_visibility(evt, { task_title: el.innerHTML, state: hiddenTasks[el.innerHTML] == 1 ? 1 : 0 }); });
         });
     }
 }
 
 function init_interface() {
-    $($('.tab-content-career h2')[0]).after('<div class="tau_station_hidden_tasks_btn_edit"><span id="tau_station_hidden_task_btn_edit">Edit task visibility</span></div>');
-    $('#tau_station_hidden_task_btn_edit').on('click', toggle_edit_interface);
+    $($('.tab-content-career h2')[0]).after('<div class="tau_station_hidden_tasks_btn_edit" id="tau_station_hidden_tasks_btn_edit"><span id="tau_station_hidden_tasks_btn_edit_label">Edit task visibility</span></div>');
+    $('#tau_station_hidden_tasks_btn_edit').click(toggle_edit_interface);
+}
+
+function add_css(css){
+    // Ref: https://stackoverflow.com/questions/3922139/add-css-to-head-with-javascript
+    var head = document.getElementsByTagName('head')[0];
+    var s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    if (s.styleSheet) {   // IE
+        s.styleSheet.cssText = css;
+    } else {              // the world
+        s.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(s);
 }
 
 (function() {
