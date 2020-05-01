da9733fdde1eab6b82d71f28137201c97ecf0f26
Dotsent <sergey.kudriavtsev@gmail.com>
2020-05-01 15:19:58 +0200

    Automated Popup Dismisser: work around more types of popups

--- automated_popup_dismisser.user.js
+++ automated_popup_dismisser.user.js
@@ -1,17 +1,29 @@
 // ==UserScript==
 // @name         Automated Popup Dismisser
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Automatically closes any pop-up windows related to started/finished repair etc.
 // @author       Dotsent
 // @match        https://alpha.taustation.space/*
 // @grant        none
 // ==/UserScript==
 
+var interval;
+
+function hideDialog() {
+    if ($('button.dialog-dismiss:visible, .dialog-bottom .btn-control span:visible').length > 0) {
+        $('button.dialog-dismiss:visible, .dialog-bottom .btn-control span:visible').trigger("click");
+        window.clearInterval(interval);
+        window.setTimeout(startMonitoring, 10000); // Wait with monitoring to prevent page reload click-loop on slow devices or connections
+    }
+}
+
+function startMonitoring() {
+    interval = window.setInterval(hideDialog, 500);
+}
+
 (function() {
     'use strict';
 
-    window.setInterval(function() {
-        $('button.dialog-dismiss').trigger("click");
-    }, 500);
+    startMonitoring();
 })();
